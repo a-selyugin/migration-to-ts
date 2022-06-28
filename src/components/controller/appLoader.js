9d558cece4e29b99ccb5cc817e8518d1faad6241
a-selyugin <selyuginai@gmail.com>
2022-06-28 21:12:00 +0300

    feat: add API key

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '9931a468f12141538e711343ee55fb56', // получите свой ключ https://newsapi.org/
         });
     }
 }
