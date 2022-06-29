eb4cab96bdb7be5ddfb7c52ee556b33885531752
a-selyugin <selyuginai@gmail.com>
2022-06-29 22:21:19 +0300

    feat: add ts to webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,18 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.js'),
+    entry: path.resolve(__dirname, './src/index.ts'),
     mode: 'development',
     module: {
         rules: [
             {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                include: [path.resolve(__dirname, './src')],
             },
         ],
     },
