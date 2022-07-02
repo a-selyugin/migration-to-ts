4b353b3dc3d81a841e555d7288469c8f348aeda3
a-selyugin <selyuginai@gmail.com>
2022-07-02 13:27:33 +0300

    feat: migrate sources.js to ts

---
+++ src/components/view/sources/sources.ts
@@ -0,0 +1,22 @@
+import './sources.css';
+import '../../../types/RespSources';
+
+class Sources {
+    draw(data: Array<Source>): void {
+        const fragment: DocumentFragment = document.createDocumentFragment();
+        const sourceItemTemp: HTMLTemplateElement = document.querySelector('#sourceItemTemp') as HTMLTemplateElement;
+
+        data.forEach((item: Source) => {
+            const sourceClone: HTMLTemplateElement = sourceItemTemp.content.cloneNode(true) as HTMLTemplateElement;
+
+            (sourceClone.querySelector('.source__item-name') as HTMLDivElement).textContent = item.name;
+            (sourceClone.querySelector('.source__item') as HTMLDivElement).setAttribute('data-source-id', item.id);
+
+            fragment.append(sourceClone);
+        });
+
+        (document.querySelector('.sources') as HTMLDivElement).append(fragment);
+    }
+}
+
+export default Sources;
