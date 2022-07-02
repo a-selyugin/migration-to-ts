f7802a70740652b422758467cc757838a10fddfa
a-selyugin <selyuginai@gmail.com>
2022-07-02 17:43:47 +0300

    feat: migrate app.js to ts

---
+++ src/components/app/app.ts
@@ -0,0 +1,21 @@
+import AppController from '../controller/controller';
+import { AppView } from '../view/appView';
+
+class App {
+    controller: AppController;
+    view: AppView;
+
+    constructor() {
+        this.controller = new AppController();
+        this.view = new AppView();
+    }
+
+    start(): void {
+        (document
+            .querySelector('.sources') as Element)
+            .addEventListener('click', (e: Event) => this.controller.getNews(e, (data: RespNews) => this.view.drawNews(data)));
+        this.controller.getSources((data: RespSources) => this.view.drawSources(data));
+    }
+}
+
+export default App;
