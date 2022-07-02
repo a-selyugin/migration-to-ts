35994a4ca9fd007f59992555a68e68073b23d661
a-selyugin <selyuginai@gmail.com>
2022-07-02 16:58:56 +0300

    feat: migrate appLoader.js to ts

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -1,6 +1,6 @@
 import Loader from './loader';
 
-class AppLoader extends Loader {
+class AppLoader extends Loader {    
     constructor() {
         super('https://newsapi.org/v2/', {
             apiKey: '9931a468f12141538e711343ee55fb56', // получите свой ключ https://newsapi.org/
