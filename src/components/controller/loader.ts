59870495bb612f3542929bb25880119482cfc380
a-selyugin <selyuginai@gmail.com>
2022-07-02 16:55:05 +0300

    feat: migrate loader.js to ts

--- src/components/controller/loader.ts
+++ src/components/controller/loader.ts
@@ -1,19 +1,26 @@
+import '../../types/RespNews';
+import '../../types/RespSources';
+import { Options } from '../../types/Interfaces';
+
 class Loader {
-    constructor(baseLink, options) {
+    baseLink: string;
+    options: Options;
+
+    constructor(baseLink: string, options: Options) {
         this.baseLink = baseLink;
         this.options = options;
     }
 
-    getResp(
-        { endpoint, options = {} },
-        callback = () => {
+    getResp<T>(
+        { endpoint, options = {} }: { endpoint: string, options?: Options},
+        callback: (data?: T) => void = () => {
             console.error('No callback for GET response');
         }
-    ) {
+    ): void {
         this.load('GET', endpoint, callback, options);
     }
 
-    errorHandler(res) {
+    errorHandler<T extends Response>(res: T): T {
         if (!res.ok) {
             if (res.status === 401 || res.status === 404)
                 console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
@@ -23,8 +30,8 @@ class Loader {
         return res;
     }
 
-    makeUrl(options, endpoint) {
-        const urlOptions = { ...this.options, ...options };
+    makeUrl(options: Options, endpoint: string): string {
+        const urlOptions: { [index: string]: string } = { ...this.options, ...options };
         let url = `${this.baseLink}${endpoint}?`;
 
         Object.keys(urlOptions).forEach((key) => {
@@ -34,7 +41,7 @@ class Loader {
         return url.slice(0, -1);
     }
 
-    load(method, endpoint, callback, options = {}) {
+    load<T>(method: string, endpoint: string, callback: (data?: T) => void, options: Options = {}): void {
         fetch(this.makeUrl(options, endpoint), { method })
             .then(this.errorHandler)
             .then((res) => res.json())
